2c47ae4a9d0f94bb805be17907d7a844b111d94f
ramosjm <jjramos_2004@hotmail.com>
2019-03-21 08:53:06 +0000

    index route ready.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
 
+const createRouter = require('./helpers/create_router.js');
 
-app.use(express.static('client/public'));
+const publicPath = path.join(__dirname,'../client/public');
+app.use(express.static(publicPath));
 
-app.get('/', function (req, res) {
-  res.sendFile('index.html');
-});
+MongoClient.connect('mongodb://localhost:27017',{useNewUrlParser: true})
+  .then((client)=>{
+    const db = client.db('monopoly');
+    const tilesCollection = db.collection('tiles');
+    const monopolyRouter = createRouter(tilesCollection);
+    app.use('/api/monopoly',monopolyRouter);
+  })
+  .catch(console.error);
+
+app.use(parser.json());
 
 app.listen(3000, function () {
   console.log(`Listening on port ${ this.address().port }`);
