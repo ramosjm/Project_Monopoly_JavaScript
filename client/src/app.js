b91473f31d5a12627ab03573616716a216e06595
ramosjm <jjramos_2004@hotmail.com>
2019-03-21 14:27:18 +0000

    tiles will now come from db to front end.

--- client/src/app.js
+++ client/src/app.js
@@ -1,9 +1,12 @@
 const PubSub = require('./helpers/pub_sub.js');
+const Tile = require('./models/tile.js');
 
 document.addEventListener('DOMContentLoaded',function(){
   console.log('hiya');
 
-
+  const tileData = new Tile('http://localhost:3000/api/monopoly');
+  tileData.getData();
+  console.log(tileData.getData());
 
 
 });
--- client/src/models/tile.js
+++ client/src/models/tile.js
@@ -1,5 +1,18 @@
-const Tile = function(tile){
-  this.tileId = tileId;
+const RequestHelper = require('../helpers/request_helper.js');
+
+const Tile = function(url){
+  this.url = url;
+  this.data = [];
+};
+
+Tile.prototype.getData = function () {
+  const request = new RequestHelper(this.url);
+  return request.get()
+  .then (data => this.handleData(data));
+};
+
+Tile.prototype.handleData = function(data){
+  this.data = data
 };
 
 module.exports = Tile;
