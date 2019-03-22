c4d52f1b14e4f65b992a2f33bafbd950c9bb0866
ramosjm <jjramos_2004@hotmail.com>
2019-03-22 09:12:32 +0000

    tile tests removed.

--- client/src/models/specs/tile_spec.js
+++ client/src/models/specs/tile_spec.js
@@ -9,15 +9,6 @@ describe ('Tiles',function(){
       tile = new Tile();
     });
 
-    it('should have a tile id',function(){
-      console.log(tile);
-      assert.equal(1,tile.data[0].tile_id);
-    });
-
-    it('should have a name', function(){
-      assert.equal('GO',tile.getName())
-    });
-
 
   });
 
