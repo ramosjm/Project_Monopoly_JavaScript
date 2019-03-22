ffe7221239b99516be2d865153bbaf87769cb30f
ramosjm <jjramos_2004@hotmail.com>
2019-03-22 10:02:58 +0000

    added platey.rolldice and player.move

--- client/src/models/board.js
+++ client/src/models/board.js
@@ -3,3 +3,10 @@
 // board will have COMMUNITY chest cards.
 // board will have players and track position
 // consider a class specifically for game logic.
+
+const Player = require ('./player.js');
+
+
+const Board = function(players){
+  this.players = [players];
+};
--- client/src/models/player.js
+++ client/src/models/player.js
@@ -1,2 +1,19 @@
 //player will be able to roll dice.
-//consider a dice class.
+const Dice = require('./dice.js');
+
+const Player = function(){
+  this.position = 1;
+};
+
+Player.prototype.move = function(){
+  this.rollDice();
+  this.position = newPosition;
+
+};
+
+Player.prototype.rollDice = function(){
+  dice = new Dice();
+  newPosition = this.position + dice.roll();
+};
+
+module.exports = Player
